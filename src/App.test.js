ab04f12534f3e5f4270648c40d2c6e444c020ea8
olegshevt <olegshevt@gmail.com>
2023-05-09 15:54:57 +0200

    Write tests for creating a new game
    Initialize project#

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,54 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+describe('LiveFootballWorldCupScoreboard', () => {
+  let scoreboard;
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  beforeEach(() => {
+    scoreboard = new App();
+  });
+
+  describe('startGame', () => {
+    it('should start a new game with 0-0 score', () => {
+      const game = scoreboard.startGame('Home Team', 'Away Team');
+      expect(game).toEqual({
+        homeTeam: 'Home Team',
+        awayTeam: 'Away Team',
+        homeScore: 0,
+        awayScore: 0,
+        startTime: expect.any(Date)
+      });
+    });
+
+    it('should add the game to the scoreboard', () => {
+      scoreboard.startGame('Home Team', 'Away Team');
+      expect(scoreboard.games).toHaveLength(1);
+    });
+
+  });
+
+  describe('updateScore', () => {
+    let game;
+
+    beforeEach(() => {
+      game = scoreboard.startGame('Home Team', 'Away Team');
+    });
+
+    it('should update the score of a game', () => {
+      scoreboard.updateScore(game, 2, 1);
+      expect(game.homeScore).toBe(2);
+      expect(game.awayScore).toBe(1);
+    });
+  });
+
+  describe('finishGame', () => {
+    let game;
+
+    beforeEach(() => {
+      game = scoreboard.startGame('Home Team', 'Away Team');
+    });
+
+    it('should remove a game from the scoreboard', () => {
+      scoreboard.finishGame(game);
+      expect(scoreboard.games).toHaveLength(0);
+    });
+  });
+})
