b2705bbe65cf45f9d720e73fe39ad22e2a4910eb
olegshevt <olegshevt@gmail.com>
2023-05-09 16:02:12 +0200

    Implement create new game feature

--- src/App.js
+++ src/App.js
@@ -1,3 +1,20 @@
+class App {
+  constructor() {
+    this.games = [];
+  }
 
+  startGame(homeTeam, awayTeam) {
+    const game = {
+      homeTeam,
+      awayTeam,
+      homeScore: 0,
+      awayScore: 0,
+      startTime: new Date()
+    };
+    this.games.push(game);
+    return game;
+  }
+
+}
 
 
